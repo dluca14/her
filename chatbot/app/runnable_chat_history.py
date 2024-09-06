a25d607b7f1dc9b06f8de02f79135e7d0ff18cf8
dluca14 <dluca14@protonmail.com>
2024-09-06 17:10:01 +0300

    Use REDIS for session history

--- chatbot/app/runnable_chat_history.py
+++ chatbot/app/runnable_chat_history.py
@@ -1,14 +1,49 @@
+import dotenv
+from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_openai import ChatOpenAI
 from langchain_core.runnables.history import RunnableWithMessageHistory
-from langchain_community.chat_message_histories import ChatMessageHistory
-import dotenv
+from langchain_community.chat_message_histories import ChatMessageHistory, RedisChatMessageHistory
 
 
 dotenv.load_dotenv()
+# docker run -d -p 6379:6379 -p 8001:8001 redis/redis-stack:latest
+REDIS_URL = 'redis://localhost:6379/0'
+
+# def get_session_history(session_id: str) -> BaseChatMessageHistory:
+#     if session_id not in store:
+#         store[session_id] = ChatMessageHistory()
+#     return store[session_id]
+def get_session_history(session_id) -> RedisChatMessageHistory:
+    return RedisChatMessageHistory(session_id, url=REDIS_URL)
+
+def summarize_messages(chain_input: dict, config: dict) -> bool:
+    # stored_messages = demo_ephemeral_chat_history.messages
+    session_history = get_session_history(config["session_id"])
+    stored_messages = session_history.messages
+    if len(stored_messages) == 0:
+        return False
+    summarization_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("placeholder", "{chat_history}"),
+            (
+                "user",
+                "Distill the above chat messages into a single summary message. Include as many specific details as you can.",
+            ),
+        ]
+    )
+    summarization_chain = summarization_prompt | chat
+    summary_message = summarization_chain.invoke({"chat_history": stored_messages})
+    # demo_ephemeral_chat_history.clear()
+    # demo_ephemeral_chat_history.add_message(summary_message)
+    session_history.clear()
+    session_history.add_message(summary_message)
+
+    return True
 
 chat = ChatOpenAI(model="gpt-3.5-turbo-0125")
+demo_ephemeral_chat_history = ChatMessageHistory()
 
 prompt = ChatPromptTemplate.from_messages(
     [
@@ -23,38 +58,21 @@ prompt = ChatPromptTemplate.from_messages(
 
 chain = prompt | chat
 
-demo_ephemeral_chat_history = ChatMessageHistory()
-
-
-def summarize_messages(chain_input):
-    stored_messages = demo_ephemeral_chat_history.messages
-    if len(stored_messages) == 0:
-        return False
-    summarization_prompt = ChatPromptTemplate.from_messages(
-        [
-            ("placeholder", "{chat_history}"),
-            (
-                "user",
-                "Distill the above chat messages into a single summary message. Include as many specific details as you can.",
-            ),
-        ]
-    )
-    summarization_chain = summarization_prompt | chat
-    summary_message = summarization_chain.invoke({"chat_history": stored_messages})
-    demo_ephemeral_chat_history.clear()
-    demo_ephemeral_chat_history.add_message(summary_message)
-
-    return True
-
 chain_with_message_history = RunnableWithMessageHistory(
     chain,
-    lambda session_id: demo_ephemeral_chat_history,
+    # lambda session_id: demo_ephemeral_chat_history,
+    get_session_history,
     input_messages_key="input",  # specifies which part of the input should be tracked and stored in the chat history. In this example, we want to track the string passed in as input.
     history_messages_key="chat_history",  # specifies what the previous messages should be injected into the prompt as. Our prompt has a MessagesPlaceholder named chat_history, so we specify this property to match.
 )
 
+# https://python.langchain.com/v0.2/docs/how_to/chatbots_memory/#summary-memory
 chain_with_summarization = (
-    RunnablePassthrough.assign(messages_summarized=summarize_messages)| chain_with_message_history
+    # RunnablePassthrough.assign(messages_summarized=summarize_messages)
+    RunnablePassthrough.assign(
+        messages_summarized=lambda chain_input, config: summarize_messages(chain_input, config["configurable"])
+    )
+    | chain_with_message_history
 )
 # chain_with_message_history.invoke(
 #     {"input": "Translate this sentence from English to French: I love programming."},
@@ -72,8 +90,8 @@ def run_chat():
 
         user_inp = {'input': user_input}
         response = chain_with_summarization.invoke(
-            user_inp,
-            {"configurable": {"session_id": "unused"}},
+            input=user_inp,
+            config={"configurable": {"user_id": "user1", "session_id": "session1"}},
         )
         print('ChatGPT:', response.content)
 
